560b90caf24ba4323358988fecec54d4a67bfd68
deinhofer <deinhofer.m@gmail.com>
2026-03-04 23:51:25 +0100

    fixed keyReleaseAllAction in keyboard move mode; optimized update intervals for mouse and keyboard; fixed AHRS init; improved smooth mouse movement

--- Code/HEIDI_Final_Code.js
+++ Code/HEIDI_Final_Code.js
@@ -117,6 +117,7 @@ function clickButtonAction(b) { try { HID.clickButton(b); } catch(e){digitalPuls
 function tapKeyAction(k) { try { HID.tapKey(k); } catch(e){digitalPulse(LED1,1,[10]); } }
 function holdKeyAction(k) { try { HID.keyDown(k); } catch(e){digitalPulse(LED1,1,[10]); } }
 function releaseKeyAction(k) { try { HID.keyUp(k); } catch(e){digitalPulse(LED1,1,[10]); } }
+function releaseKeyAllAction() { try { HID.keyUp(HID.KEY.ALL); } catch(e){digitalPulse(LED1,1,[10]);digitalPulse(LED2,1,[10]); } }
 
 //
 // Direct action executor (for physical buttons)
@@ -165,23 +166,14 @@ var currentMode = InputMode.MOUSE;
 function changeMode() {
 
   // Always release all the directional keys
-  releaseKeyAction(HID.KEY.W);
-  releaseKeyAction(HID.KEY.A);
-  releaseKeyAction(HID.KEY.S);
-  releaseKeyAction(HID.KEY.D);
+  releaseKeyAllAction();
 
   // Change modality
   currentMode = (currentMode === InputMode.MOUSE) ? InputMode.MOVEMENT : InputMode.MOUSE;
-}
-
-//
-// AHRS (INIT ONCE)
-//
-var ahrsReady = false;
-function initAHRS() {
-  if (ahrsReady) return;
-  AHRS.init();
-  ahrsReady = true;
+  //show current mode with LED indicators
+  (currentMode === InputMode.MOUSE) ? digitalPulse(LED2,1,[800,100,800,100,800]) : digitalPulse(LED3,1,[800,100,800,100,800]);
+  stopTilt();
+  startTilt(mode2Interval[currentMode]);
 }
 
 //
@@ -203,32 +195,36 @@ function calibrateMouse() {
 function updateMouseMovementDegree(a) {
   let dx = a.pitch - x_calib;
   let dy = a.roll  - y_calib;
+  //let speed=10;
+  const sensitivity = 10; // Adjust sensitivity as needed (lower value = higher sensitivity)
+  const max_speed = 25;
 
   if (Math.abs(dx) < DEAD_ZONE) dx = 0;
   if (Math.abs(dy) < DEAD_ZONE) dy = 0;
 
+  var speed_roll = Math.abs(dx) - sensitivity;
+  var speed_pitch = Math.abs(dy) - sensitivity;
+  speed_roll = speed_roll > max_speed ? max_speed : speed_roll;
+  speed_pitch = speed_pitch > max_speed ? max_speed : speed_pitch;
+
+
   if (currentMode === InputMode.MOUSE) {
-
-    // Always release all the directional keys
-    releaseKeyAction(HID.KEY.W);
-    releaseKeyAction(HID.KEY.A);
-    releaseKeyAction(HID.KEY.S);
-    releaseKeyAction(HID.KEY.D);
-
-    let x = dx ? (dx > 0 ? 20 : -20) : 0;
-    let y = dy ? (dy > 0 ? 20 : -20) : 0;
+    let x = dx ? (dx > 0 ? speed_roll : -speed_roll) : 0;
+    let y = dy ? (dy > 0 ? speed_pitch : -speed_pitch) : 0;
 
     if (x || y) moveMouseAction(x, y, 0);
   }
 
   else { // MOVEMENT
-    if (dy > 0) { releaseKeyAction(HID.KEY.W); holdKeyAction(HID.KEY.S); }
-    else if (dy < 0) { releaseKeyAction(HID.KEY.S); holdKeyAction(HID.KEY.W); }
-    else { releaseKeyAction(HID.KEY.W); releaseKeyAction(HID.KEY.S); }
+    //releaseKeyAllAction();
+    let holdKey=-1;
+    if (dy > 0) { holdKey=HID.KEY.S; }
+    else if (dy < 0) { holdKey=HID.KEY.W; }
 
-    if (dx > 0) { releaseKeyAction(HID.KEY.A); holdKeyAction(HID.KEY.D); }
-    else if (dx < 0) { releaseKeyAction(HID.KEY.D); holdKeyAction(HID.KEY.A); }
-    else { releaseKeyAction(HID.KEY.A); releaseKeyAction(HID.KEY.D); }
+    if (dx > 0) { holdKey=HID.KEY.D; }
+    else if (dx < 0) { holdKey=HID.KEY.A; }
+
+    if(holdKey>0) { holdKeyAction(holdKey); } else { releaseKeyAllAction();}
   }
 }
 
@@ -236,14 +232,19 @@ function updateMouseMovementDegree(a) {
 // Tilt polling
 //
 var tiltInterval;
-const mouseMoveInterval = 100;
+//use two different intervals [ms] as in keyboard movement mode we hold keys anyway
+const mouseMoveInterval = 25;
+const keyboardMoveInterval = 250;
+const mode2Interval=[mouseMoveInterval,keyboardMoveInterval];
 
-function startTilt() {
-  if (tiltInterval) return;
+function startTilt(updateInterval) {
+  //if (tiltInterval) return;
+  stopTilt();
+  
   tiltInterval = setInterval(() => {
     let o = AHRS.getOrientationDegree();
     updateMouseMovementDegree(o);
-  }, mouseMoveInterval);
+  }, updateInterval);
 }
 
 function stopTilt() {
@@ -263,9 +264,8 @@ NRF.on('connect', function (addr) {
   NRF.setSecurity({ mitm: false, display: false, keyboard: false });
 
   // Enable accelerometer with default frequency (26Hz) only when connected
-  initAHRS();
-  Puck.accelOn(26);  
-  startTilt();
+  AHRS.init();
+  startTilt(mode2Interval[currentMode]);
 });
 
 NRF.on('disconnect', function (reason) {
@@ -274,7 +274,7 @@ NRF.on('disconnect', function (reason) {
   digitalWrite([LED1,LED2,LED3],0);
 
   // Turn off accelerometer to save power when not connected
-  Puck.accelOff();  
+  Puck.accelOff();
   stopTilt();
 });
 
